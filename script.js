77389cf8d2efa720826e495893d911bd93c970a9
David2561 <davidtkf256@gmail.com>
2021-10-01 09:59:34 -0300

    add eraser button

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 
-const canvas = document.querySelector('.canvas');
+let color = 'black';
+let rainbow = false;
 
+const canvas = document.querySelector('.canvas');
+const btnColor = document.querySelector('#color');
+const btnRainb = document.querySelector('#rainbow');
+const btnErasr = document.querySelector('#eraser')
+const btnReset = document.querySelector('#reset');
+const btnResiz = document.querySelector('#resize');
+
+
+
+btnErasr.addEventListener('click', ()=>{
+    color = 'white';
+})
+btnReset.addEventListener('click',resetCanvas);
 
 
 InitializeCanvas(16);
+resetCanvas();
+
 function InitializeCanvas(canvasSize){
-    canvasSize *= canvasSize;
     
+    let size = canvas.clientHeight;
+    let height = size/canvasSize;
+    let width = height;
+    canvasSize *= canvasSize;
+
     for (let i = 0; i < canvasSize; i++) {
         const div = document.createElement('div');
         div.classList.add('pixel');
+        div.style.height= height.toString()+ 'px';
+        div.style.width = width.toString()+ 'px';
         div.addEventListener('mouseover',changeColor);        
         canvas.appendChild(div);
     }
@@ -17,5 +39,23 @@ function InitializeCanvas(canvasSize){
 
 function changeColor(e){
     let target = e.target;
-    target.style.backgroundColor = "black";
+    if (!rainbow) {
+        target.style.backgroundColor = color;    
+    }
+    else{
+        let randcode = Math.floor( Math.random()*16);
+        let rainbowColor = '#' + randcode.toString() + 
+        randcode.toString() + randcode.toString();
+        
+        target.style.backgroundColor = rainbowColor;
+    }
 }
+
+function resetCanvas(){
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {
+        pixel.style.backgroundColor='white';
+    });
+}
+
+

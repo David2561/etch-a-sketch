2e914bb55d468b8143d869a8a135235dfe68d6a4
David2561 <davidtkf256@gmail.com>
2021-10-01 11:27:41 -0300

    fix mayor ui bugs

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 let color = 'black';
 let rainbow = false;
 
-const canvas = document.querySelector('.canvas');
+const colorpic = document.querySelector('#picker');
+const canvas   = document.querySelector('.canvas');
 const btnColor = document.querySelector('#color');
 const btnRainb = document.querySelector('#rainbow');
 const btnErasr = document.querySelector('#eraser')
@@ -10,15 +11,17 @@ const btnReset = document.querySelector('#reset');
 const btnResiz = document.querySelector('#resize');
 
 
-
-btnErasr.addEventListener('click', ()=>{
-    color = 'white';
-})
+colorpic.addEventListener('change', setColor);
+btnColor.addEventListener('click', setColor);
+btnRainb.addEventListener('click', setRainbow);
+btnErasr.addEventListener('click', erasorColor);
 btnReset.addEventListener('click',resetCanvas);
+btnResiz.addEventListener('click', resizeCanvas)
 
 
 InitializeCanvas(16);
 resetCanvas();
+setColor('black');
 
 function InitializeCanvas(canvasSize){
     
@@ -34,6 +37,7 @@ function InitializeCanvas(canvasSize){
         div.style.width = width.toString()+ 'px';
         div.addEventListener('mouseover',changeColor);        
         canvas.appendChild(div);
+        
     }
 }
 
@@ -41,12 +45,15 @@ function changeColor(e){
     let target = e.target;
     if (!rainbow) {
         target.style.backgroundColor = color;    
+
     }
     else{
-        let randcode = Math.floor( Math.random()*16);
-        let rainbowColor = '#' + randcode.toString() + 
-        randcode.toString() + randcode.toString();
-        
+
+        let randR = Math.floor( Math.random()*16);
+        let randG = Math.floor( Math.random()*16);
+        let randB = Math.floor( Math.random()*16);
+        let rainbowColor = '#' + randR.toString() + 
+        randG.toString() + randB.toString();
         target.style.backgroundColor = rainbowColor;
     }
 }
@@ -57,5 +64,44 @@ function resetCanvas(){
         pixel.style.backgroundColor='white';
     });
 }
+function setColor(){
+    color = colorpic.value.toString();
+    rainbow = false;
+    btnRainb.classList.remove('selected');
+    btnErasr.classList.remove('selected');
+    btnColor.classList.add('selected');
+}
 
+function erasorColor(){
+    color = 'white';
+    rainbow = false;
+    btnRainb.classList.remove('selected');
+    btnErasr.classList.add('selected');
+    btnColor.classList.remove('selected');
+}
+function setRainbow(){
+    rainbow = true;
+    btnRainb.classList.add('selected');
+    btnErasr.classList.remove('selected');
+    btnColor.classList.remove('selected');
+}
+function removeCanvas(){
+    
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {
+        canvas.removeChild(pixel);
+    });
+}
 
+function resizeCanvas(){
+    let res = prompt("Instert canvas resolution(max. 100)")
+    if(res > 0 && res <= 100)
+    {
+        removeCanvas();
+        InitializeCanvas(res);
+        resetCanvas();
+    }
+    else{
+        alert(res + " is not a valid input.")
+    }
+}

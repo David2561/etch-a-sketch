1018f66675f8b1d475d479e10bf74d06dfd7942b
David2561 <davidtkf256@gmail.com>
2021-10-01 07:37:24 -0300

    fix canvas

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 
-InitializeCanvas(256);
+const canvas = document.querySelector('.canvas');
+
+
+
+InitializeCanvas(16);
 function InitializeCanvas(canvasSize){
-    const canvas = document.querySelector('.canvas');
+    canvasSize *= canvasSize;
+    
     for (let i = 0; i < canvasSize; i++) {
         const div = document.createElement('div');
         div.classList.add('pixel');
+        div.addEventListener('mouseover',changeColor);        
         canvas.appendChild(div);
     }
+}
+
+function changeColor(e){
+    let target = e.target;
+    target.style.backgroundColor = "black";
 }
